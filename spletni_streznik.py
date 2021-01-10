35521d61285cf59f5f35c6633cf6744a2392dcd5
Globocnik0 <ales.globocnik0@gmail.com>
2021-01-10 23:14:37 +0100

    Gumb za oddajo datotek

--- spletni_streznik.py
+++ spletni_streznik.py
@@ -4,4 +4,20 @@ import bottle
 def zacetek():
     return bottle.template('naslov.tpl')
 
+
+@bottle.post('/upload')
+def do_upload():
+    name = bottle.request.forms.name
+    data = bottle.request.files.data
+    if name and data and data.file:
+        filename = data.filename
+
+        with open(filename, "wb") as file:
+            Data = data.file.read()
+            if type(Data) == bytes: file.write(Data)
+            elif type(Data) == str: file.write(Data.encode("utf-8"))
+
+        return "Hello %s! You uploaded %s (%d bytes)." % (name, filename, len(Data))
+    return "You missed a field."
+
 bottle.run(debug=True, reloader=True)
--- views/naslov.tpl
+++ views/naslov.tpl
@@ -4,5 +4,12 @@
     </head>
     <body>
         <h1>Pozdravljen na strani, kjer se ustvarjanje grafov začne.</h1>
+
+        <h2>
+        <form action="/upload" method="post" enctype="multipart/form-data">
+            <input type="text" name="name" />
+            <input type="file" name="data" />
+        </form>
+        </h2>
     </body>
 </html>
